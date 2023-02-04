bc8fa38e156d6044018ba196e6dbe8c9a3aac170
stigsanek <stigsanek@yandex.ru>
2023-02-04 10:47:42 +0400

    Add test endpoint

---
+++ poll_api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from poll_api.api.api_v1.api import api_router
+from poll_api.config import settings
+
+app = FastAPI(
+    debug=settings.DEBUG,
+    title=settings.PROJECT_NAME,
+    openapi_url=f'{settings.API_V1}/openapi.json'
+)
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(url) for url in settings.BACKEND_CORS_ORIGINS],
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+
+app.include_router(api_router, prefix=settings.API_V1)
