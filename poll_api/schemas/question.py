e938f8015f7b8399b338edf085afbb0cbd86e5f8
stigsanek <stigsanek@yandex.ru>
2023-02-07 11:52:41 +0400

    Update question schemas

--- poll_api/schemas/question.py
+++ poll_api/schemas/question.py
@@ -6,27 +6,31 @@ from pydantic import BaseModel, Field
 from poll_api.schemas.choice import Choice, ChoiceBase
 
 
-class QuestionCreate(BaseModel):
-    """Properties to receive via API on creation
+class QuestionBase(BaseModel):
+    """Base shared properties
     """
     text: str = Field(max_length=150)
+
+
+class QuestionCreate(QuestionBase):
+    """Properties to receive via API on creation
+    """
     choices: List[ChoiceBase]
 
 
-class QuestionUpdate(BaseModel):
-    """Properties to receive via API on update
-    """
-    text: str = Field(max_length=150)
-
-
-class Question(QuestionCreate):
-    """Properties to return via API
+class Question(QuestionBase):
+    """Properties to return via API for question list
     """
     id: int
     created_at: datetime
     updated_at: datetime
     user_id: int
-    choices: List[Choice]
 
     class Config:
         orm_mode = True
+
+
+class QuestionItem(Question):
+    """Properties to return via API for one question
+    """
+    choices: List[Choice]
